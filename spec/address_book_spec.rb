452cef818c928e408d50a19a6c1058fcc6ccdaff
Jeff Wolfram <jeffwolfram@gmail.com>
2017-02-08 10:53:34 -0800

    final touches on tests

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -40,10 +40,16 @@ require_relative '../models/address_book'
    describe "#remove_entry" do
      it "removes selected entry from the address book" do
        book = AddressBook.new
-       book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@loveless.com')
-       book.remove_entry(name, phone_number, email)
-       expect(book.entries.size).to eq(0)
-       expect(book.entries.first.name).to eq('')
+       book.add_entry('Michael Faraday', '010.210.5181', 'michael.faraday@faraday.com')
+       name = "Ada Lovelace"
+       phone_number = "010.012.1815"
+       email_address = "augusta.king@loveless.com"
+       book.add_entry(name, phone_number, email_address)
+
+       expect(book.entries.size).to eq 2
+       book.remove_entry(name, phone_number, email_address)
+       expect(book.entries.size).to eq 1
+       expect(book.entries.first.name).to eq('Michael Faraday')
 
      end
    end
