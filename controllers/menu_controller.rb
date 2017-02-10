9e5e4215669cde3160f00b7934c850f5e530f0a9
Jeff Wolfram <jeffwolfram@gmail.com>
2017-02-10 14:04:06 -0800

    added a few things to the menu and set up delete all

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -15,6 +15,7 @@ end
     puts "4 - Import entries from a CSV"
     puts "5 - View entry number"
     puts "6 - Exit"
+    puts "7 - DELETE ALL ENTRIES!!!!!!!"
     print "Enter your selection: "
 
     selection = gets.to_i
@@ -43,11 +44,14 @@ end
     when 6
       puts "Good-bye"
       exit(0)
+    when 7
+      puts "delete all entries"
+      nuke
     else
       system "clear"
       puts "Sorry, that is not a valid input"
       main_menu
-    end
+  end
   end
   def view_all_entries
     address_book.entries.each do |entry|
@@ -75,6 +79,52 @@ end
     system "clear"
     puts "New entry created"
   end
+
+  #import csv file
+  def read_csv
+    print "Enter CSV file to import: "
+    file_name = gets.chomp
+
+    if file_name.empty?
+      system "clear"
+      puts "No CSV file read"
+      main_menu
+    end
+
+    begin
+      entry_count = address_book_from_csv(file_name).count
+      system "clear"
+      puts "#{entry_count} new entries added from #{file_name}"
+    rescue
+      puts "#{file_name} is not a valid CSV file, please enter the name of a valid CSV file"
+      read_csv
+    end
+  end
+
+  def delete_entry(entry)
+    address_book.entries.delete(entry)
+    puts "#{entry.name} has been deleted"
+
+  end
+  def nuke
+    puts "Are you sure you want to delete the whole address book?? (yes/no)"
+    answer = gets.chomp
+    if answer == "yes"
+      the_count = address_book.entries.count
+    address_book.entries.clear
+    system "clear"
+    puts "You have deleted #{the_count} entries"
+    main_menu
+  elsif answer == "no"
+    system "clear"
+    main_menu
+  else
+    puts "invalid selection"
+    system "clear"
+    main_menu
+    end
+
+  end
   def entry_number
 
       puts "Select entry number"
@@ -84,12 +134,67 @@ end
     else
       puts "You have picked an invalid number"
       entry_number
-    end
+      end
+
+  end
+
+  def edit_entry(entry)
+    print "Updated name: "
+    name = gets.chomp
+    print "Updated Phone Number: "
+    phone_number = gets.chomp
+    print "updated email: "
+    email = gets.chomp
+
+    entry.name = name if !name.empty?
+    entry.phone_number = phone_number if !phone_number.empty?
+    entry.email = email if !email.empty?
+    system "clear"
+
+    puts "Updated entry: "
+    puts entry
 
   end
 
   def search_entries
+print "Search by name: "
+name = gets.chomp
+match = address_book.binary_search(name)
+system "clear"
+if match
+  puts match.to_s
+  search_submenu(match)
+else
+  puts "No match found for #{name}"
+end
+
   end
+  def search_submenu(entry)
+    puts "\nd - delete entry"
+    puts "e - edit this entry"
+    puts " m - return to main menu"
+
+    selection = gets.chomp
+    case selection
+    when "d"
+      system "clear"
+      delete_entry(entry)
+      main_menu
+    when "e"
+      edit_entry(entry)
+      system "Clear"
+      main_menu
+    when "m"
+      system "Clear"
+      main_menu
+    else
+      system "clear"
+      puts "#{selection} is not a valid input"
+      puts entry.to_s
+      search_submenu(entry)
+    end
+  end
+
   def entry_submenu(entry)
      # #16
      puts "n - next entry"
@@ -102,7 +207,10 @@ end
      case selection
        when "n"
        when "d"
+         delete_entry(entry)
        when "e"
+         edit_entry(entry)
+         entry_submenu(entry)
        when "m"
          system "clear"
          main_menu
@@ -111,6 +219,6 @@ end
          puts "#{selection} is not a valid input"
          entry_submenu(entry)
      end
-   end
+  end
 
 end
